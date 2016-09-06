d4b76cea63f611c7d02e09e0644a9084dfe65d5d
bernieyangmh <berniey@163.com>
2016-09-06 23:19:32 +0800

    11.19 method of POST

--- west/views.py
+++ west/views.py
@@ -2,16 +2,14 @@
 # -*- coding: utf-8 -*-
 # Create your views here.
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_protect
 
 from django.http import HttpResponse
 from west.models import Character
 
 def staff(request):
     staff_list = Character.objects.all()
-    staff_str  = map(str, staff_list)
-    context = {'label': ' '.join(staff_str)}
-    return HttpResponse("<p>" + ' '.join(staff_str) + "</p>")
-
+    return render(request, 'templay.html', {'staffs': staff_list})
 
 def first_page(request):
     return HttpResponse("<p>西餐</p>")
@@ -20,3 +18,13 @@ def templay(request):
     context = {}
     context['label'] = 'Hello World!'
     return render(request, 'templay.html', context)
+
+def form(request):
+    return render(request, 'form.html')
+
+@csrf_protect
+def investigate(request):
+    ctx ={}
+    if request.POST:
+        ctx['rlt'] = request.POST['staff']
+    return render(request, "investigate.html", ctx)
